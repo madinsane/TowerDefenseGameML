79b65890138cf41dc5b810be22756edd637d6414
madinsane <abrownlow1@gmail.com>
2019-03-04 02:41:59 +0000

    Added mouseOver text to shop

---
+++ Assets/Scripts/ShopText.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopText : MonoBehaviour
+{
+    public Text text;
+    public void ChangeText(string newText)
+    {
+        newText = newText.Replace("\\n", "\n");
+        text.text = newText;
+    }
+}
